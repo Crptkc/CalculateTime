6d9b8b121b4987101fd99e2b226f750325dee876
Crptkc <58362274+Crptkc@users.noreply.github.com>
2023-07-12 17:15:04 +0000

    Finished

--- Time.h
+++ Time.h
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+void getTime(int &h,int &m);
 int subtract(int h1,int m1,int h2,int m2);
 int subtract(int m1,int m2);
 
@@ -9,21 +10,11 @@ void getTime(int &h,int &m){
 }
 
 int subtract(int h1, int m1, int h2, int m2) {
-    int diff1 = m2 - m1;
-    if (diff1 < 0) {
-        h2--;
-    }
-    int diffh = h2 - h1;
-    if (diffh < 0) {
-      diffh+= 24;
-    }
+int diffh = (h2 - h1 + 48 - (m2 < m1)) % 24;
     return diffh;
 }
 
 int subtract(int m1, int m2) {
-    int diff = m2 - m1;
-    if (diff < 0) {
-        diff += 60;
-    }
-    return diff;
+    int diffm = (60 + m2 - m1) % 60;
+    return diffm;
 }
